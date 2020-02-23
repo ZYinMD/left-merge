85cb128f012f3b9b1a65b10c54f1f1a0dc3971c9
Zhi Yin <Z.Yin.MD@gmail.com>
2020-02-23 00:21:08 -0500

    improve project root setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,21 @@ module.exports = {
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
 		'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-		'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends arr
-		'plugin:jest/recommended', // without it, "describe" etc will be undefined.
+		'plugin:jest/recommended', // without it, "describe" etc will be undefined. (unless I set "jest" in env?)
 	],
 	plugins: ['jest'],
-	parserOptions: {
-		ecmaVersion: 2018,
+	env: {
+		browser: true,
+		node: true,
+		jest: true,
 	},
-	rules: {},
+	parserOptions: {
+		sourceType: 'module',
+		es2020: true,
+	},
+	rules: {
+		'@typescript-eslint/no-explicit-any': [0],
+		'@typescript-eslint/no-use-before-define': ['error', { functions: false }],
+		'@typescript-eslint/explicit-function-return-type': [0],
+	},
 };
